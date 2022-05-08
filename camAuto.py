206c81c938048056981ef334674ec523139c379f
TanavSaraf <75167346+TanavSaraf@users.noreply.github.com>
2022-05-08 16:23:06 +0530

    Add files via upload
    ..

---
+++ camAuto.py
@@ -0,0 +1,47 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time = time.time()
+
+def take_snapshot():
+    number = random.randint(0,100)
+    #initializing cv2
+    videoCaptureObject = cv2.VideoCapture(0)
+    result = True
+    while(result):
+        #read the frames while the camera is on
+        ret,frame = videoCaptureObject.read()
+        #cv2.imwrite() method is used to save an image to any storage device
+        img_name = "img"+str(number)+".png"
+        cv2.imwrite(img_name, frame)
+        start_time = time.time
+        result = False
+    return img_name
+    print("snapshot taken")
+    # releases the camera
+    videoCaptureObject.release()
+    #closes all the window that might be opened while this process
+    cv2.destroyAllWindows()
+
+
+
+def upload_file(img_name):
+    access_token="sl.BHPB56Mrj4I4TioCsO3ts2y-RiUXv_rBDM_8Gh4iedE4O9hkEDlHeawclvHMDBmCcdwQifcHlXyeC3V9YpZ2616pK84q1LJ5ge8TSUGHShGZdNBYU5dmZPl5ZM-eVzeQpBO4fz2R6La7"          
+    file =img_name
+    file_from = file
+    file_to="/testFolder/"+(img_name)
+    dbx = dropbox.Dropbox(access_token)
+
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode.overwrite)
+        print("file uploaded")
+
+
+def main():
+    
+    name = take_snapshot()
+    upload_file(name)
+
+main()
